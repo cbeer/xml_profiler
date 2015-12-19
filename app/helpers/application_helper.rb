2a75fd7d2eeaf9930335ffad42a71e9fb621e822
Chris Beer <cabeer@stanford.edu>
2015-12-19 10:01:27 -0800

    Add dynamic blacklight configuration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def render_document_field(options = {})
+    arr = Array(options[:value]).map do |v|
+      content_tag :pre, v
+    end
+
+    safe_join arr, ' '
+  end
 end
